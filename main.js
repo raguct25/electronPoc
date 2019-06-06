5c0ef0eeb7e6b2852233fefb5fe054983746357b
raguct25 <raguct2@gmail.com>
2019-06-06 18:45:18 +0530

    custom menu applicationmenu and context menu

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow, ipcMain, dialog} = require('electron');
+const {app, BrowserWindow, ipcMain, dialog,  Menu, shell, MenuItem, globalShortcut} = require('electron');
 
 let indexWindow;
 
@@ -14,7 +14,7 @@ function createWindow () {
     show: false
   })
 
-  indexWindow.loadFile('./Ipc/ipcElectron.html');
+  indexWindow.loadFile('./Menu/appMenu.html');
 
   // Open the DevTools.
 //   indexWindow.webContents.openDevTools();
@@ -46,4 +46,100 @@ function createWindow () {
 
 }
 
-app.on('ready', createWindow);
+//normal declaration
+// app.on('ready', createWindow);
+
+//custom menu declaration
+// imports { Menu, shell, MenuItem, globalShortcut}
+
+app.on('ready', function(){
+  createWindow();
+  const template = [
+    {
+      label : 'Edit',
+      submenu: [
+        {role: 'undo'},
+        {role: 'redo'},
+        {type: 'separator'},
+        {role: 'cut'},
+        {role: 'copy'},
+        {role: 'paste'},
+        {role: 'pasteandmatchstyle'},
+        {role: 'delete'},
+        { type: 'separator' },
+        {role: 'selectall'},
+      ]
+    },
+    {label : 'codingtown',
+     submenu : [
+       {
+         label: 'Raphael',
+         click: function(){
+           console.log('Raphael called')
+         }
+       },
+       {
+         type: 'separator'
+       },
+       {
+         label: 'Edison'
+       },
+       {
+         type: 'separator'
+       },
+       {
+         label: 'CT website',
+       },
+     ]
+    },
+    {label : 'ErpRoots',
+    submenu : [
+      {
+        label: 'Loganatahn'
+      },
+      {
+        label: 'Kavin'
+      }
+    ]
+  },
+  {
+    label : 'Help',
+    submenu: [
+      {
+        label: 'CT wesite',
+        click: function(){
+          shell.openExternal('http://codingtown.com/');
+        },
+        accelerator: 'CmdorCtrl + Shift + C'
+      }
+    ]
+}
+  ];
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+  const ctxMenu = new Menu();
+  ctxMenu.append(new MenuItem({
+    label: 'test1',
+    click: function(){
+      console.log('context menu clicked');
+    }
+  }));
+  ctxMenu.append(new MenuItem({
+    role: 'selectall',
+  }));
+  ctxMenu.append(new MenuItem({
+    role: 'quit',
+  }));
+
+  indexWindow.webContents.on('context-menu', function(e, params){
+    ctxMenu.popup(indexWindow, params.x, params.y);
+  })
+
+  globalShortcut.register('Alt+1', function(){
+    indexWindow.show();
+  });
+})
+
+app.on('will-quit', function(){
+  globalShortcut.unregisterAll();
+})
